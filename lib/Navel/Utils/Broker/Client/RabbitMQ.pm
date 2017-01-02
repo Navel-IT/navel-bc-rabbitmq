d476c03a8b417c83514094b78228c857f9b101ae
le-garff-yoann <pe.weeble@yahoo.fr>
2017-01-02 17:52:14 +0100

    update copyright interval: 2015-2016 -> 2015-2017

--- lib/Navel/Utils/Broker/Client/RabbitMQ.pm
+++ lib/Navel/Utils/Broker/Client/RabbitMQ.pm
@@ -1,4 +1,4 @@
-# Copyright (C) 2015-2016 Yoann Le Garff, Nicolas Boquet and Yann Le Bras
+# Copyright (C) 2015-2017 Yoann Le Garff, Nicolas Boquet and Yann Le Bras
 # navel-bc-rabbitmq is licensed under the Apache License, Version 2.0
 
 #-> BEGIN
@@ -134,7 +134,7 @@ Navel::Utils::Broker::Client::RabbitMQ
 
 =head1 COPYRIGHT
 
-Copyright (C) 2015-2016 Yoann Le Garff, Nicolas Boquet and Yann Le Bras
+Copyright (C) 2015-2017 Yoann Le Garff, Nicolas Boquet and Yann Le Bras
 
 =head1 LICENSE
 
